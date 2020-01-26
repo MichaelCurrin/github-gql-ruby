82f4f8698de9edbba201c597ed78c7d757b5464a
Michael Currin <michael.currin@gmail.com>
2020-01-26 11:36:00 +0200

    feat: Create script

---
+++ request.rb
@@ -0,0 +1,62 @@
+# Request application.
+#
+# Do a request to the GraphQL, handle errors and print the response data.
+require 'json'
+require 'faraday'
+
+
+API_URL = 'https://api.github.com/graphql'
+
+TOKEN = ENV['ACCESS_TOKEN']
+unless TOKEN
+  raise 'Env variable ACCESS_TOKEN must be set'
+end
+HEADERS = {
+  'Authorization': "token #{TOKEN}",
+  'Content-Type': "application/json",
+}
+
+QUERY = '
+  query User {
+    viewer {
+      login
+    }
+  }
+'
+
+def do_post(url, payload)
+  puts "Do POST request"
+
+  resp = Faraday.post(
+    url,
+    payload.to_json,
+    'Content-Type'=> "application/json",
+    'Authorization' => "token #{TOKEN}",
+  )
+  puts "Status: #{resp.status}"
+  if resp.status != 200
+    puts "Body:"
+    puts resp.body
+    raise "Request status: #{resp.status}"
+  end
+  resp_data = JSON.parse resp.body
+end
+
+
+def query_gql()
+  query_payload = {'query': QUERY}
+  results = do_post API_URL, query_payload
+
+  errors = results['errors']
+  if errors
+    puts 'Errors on response:'
+    puts JSON.pretty_generate errors
+    raise "Request error!"
+  end
+
+  results['data']
+end
+
+
+data = query_gql()
+puts JSON.pretty_generate data
