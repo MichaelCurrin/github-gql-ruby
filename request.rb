629aedb78412efb174998765d020c18af009efad
Michael Currin <michael.currin@gmail.com>
2020-02-27 22:19:23 +0200

    refactor: Convert to module

--- request.rb
+++ request.rb
@@ -1,57 +1,61 @@
 # Request application.
 #
 # Do a request to the GraphQL, handle errors and pretty print the response data.
+#
+# self was used in the module as per this page.
+#   https://stackoverflow.com/questions/322470/can-i-invoke-an-instance-method-on-a-ruby-module-without-including-it
 require 'json'
 require 'faraday'
 
+module Request
+  API_URL = 'https://api.github.com/graphql'
 
-API_URL = 'https://api.github.com/graphql'
-
-TOKEN = ENV['ACCESS_TOKEN']
-unless TOKEN
-  raise 'Env variable ACCESS_TOKEN must be set'
-end
-HEADERS = {
-  'Authorization': "token #{TOKEN}",
-  'Content-Type': "application/json",
-}
-
-QUERY_PATH = 'query.gql'
-QUERY = File.open(QUERY_PATH).read
-
-
-def do_post(url, payload)
-  puts "Do POST request"
-
-  resp = Faraday.post(
-    url,
-    payload.to_json,
-    HEADERS,
-  )
-  puts "Status: #{resp.status}"
-  if resp.status != 200
-    puts "Body:"
-    puts resp.body
-    raise "Request status: #{resp.status}"
+  TOKEN = ENV['ACCESS_TOKEN']
+  unless TOKEN
+    raise 'Env variable ACCESS_TOKEN must be set'
   end
-  resp_data = JSON.parse resp.body
-end
+  HEADERS = {
+    'Authorization': "token #{TOKEN}",
+    'Content-Type': "application/json",
+  }
+
+  QUERY_PATH = 'query.gql'
+  QUERY = File.open(QUERY_PATH).read
 
 
-def query_gql()
-  query_payload = {'query': QUERY}
-  results = do_post API_URL, query_payload
+  def self.do_post(url, payload)
+    puts "Do POST request"
 
-  errors = results['errors']
-  if errors
-    puts 'Errors on response:'
-    puts JSON.pretty_generate errors
-    raise "Request error!"
+    resp = Faraday.post(
+      url,
+      payload.to_json,
+      HEADERS,
+    )
+    puts "Status: #{resp.status}"
+    if resp.status != 200
+      puts "Body:"
+      puts resp.body
+      raise "Request status: #{resp.status}"
+    end
+    resp_data = JSON.parse resp.body
   end
 
-  results['data']
+  def self.query_gql()
+    query_payload = {'query': QUERY}
+    results = do_post API_URL, query_payload
+
+    errors = results['errors']
+    if errors
+      puts 'Errors on response:'
+      puts JSON.pretty_generate errors
+      raise "Request error!"
+    end
+
+    results['data']
+  end
+
+  def self.test
+    data = self.query_gql()
+    puts JSON.pretty_generate data
+  end
 end
-
-
-data = query_gql()
-puts JSON.pretty_generate data
